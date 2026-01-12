1b93d91673afabd7709a9d8e4c3d2f3a476da82a
Hassan_Achehrri <hachehri@gmail.com>
2026-01-12 13:59:33 +0000

    variables and types

---
+++ chapter_A.php
@@ -0,0 +1,21 @@
+
+
+
+
+
+<?php 
+
+  $name="Hassan";
+  $age="22";
+  $price="12.90";
+  $EstConnecte= true;
+
+  echo "Hello" . $name ."<br>";
+  echo "Your age is {$age}.<br>"  ;
+  echo "Price {$price} "."<br>";
+
+  echo "Connect :" ($EstConnecte ? "Yes" : "No")."<br>";
+   
+  var_dump($name);
+   
+?>
