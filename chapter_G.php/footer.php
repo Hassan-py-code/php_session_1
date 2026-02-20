b6fbc3959146fb58bd8f3571a6f5c50cad75d9a8
Hassan_Achehrri <hachehri@gmail.com>
2026-02-20 11:47:55 +0000

    include, require, modularization

---
+++ chapter_G.php/footer.php
@@ -0,0 +1,20 @@
+
+
+
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Document</title>
+</head>
+<body>
+
+
+  <footer>
+      <hr>
+      <p>&copy; 2025 - My site PHP</p>
+  </footer>
+    
+</body>
+</html>
---
+++ chapter_G.php/index.php
@@ -0,0 +1,23 @@
+
+
+
+<?php    
+
+   include "header.php";
+
+?>
+
+
+<main> 
+
+     <h2>Home page</h2>
+     <p>content The page </p>
+
+</main>
+
+
+<?php   
+
+  include 'footer.php';
+
+?>
